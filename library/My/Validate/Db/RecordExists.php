8cbb34ed5cc1a907e060a3fb7bc7cc54de3bbd41
anhnv <anhnv1310@gmail.com>
2014-09-19 22:49:34 +0700

    - Validate Db Record Exists

--- library/My/Validate/Db/RecordExists.php
+++ library/My/Validate/Db/RecordExists.php
@@ -8,6 +8,6 @@
 class My_Validate_Db_RecordExists extends Zend_Validate_Db_RecordExists {
 	
 	protected $_messageTemplates = array(
-		self::ERROR_NO_RECORD_FOUND => '%value% chưa được đăng ký'
+		self::ERROR_NO_RECORD_FOUND => "Tài khoản '%value%' không tồn tại"
 	);
 }
